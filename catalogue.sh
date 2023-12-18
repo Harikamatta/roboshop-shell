a0dbaa59c272ae1bf9b12d3399ba817b05184625
Harikamatta <harikamattanjc@gmail.com>
2023-12-18 21:14:14 +0530

    roboshop shell scripts

---
+++ catalogue.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+dnf module disable nodejs -y &>> $LOGFILE
+VALIDATE $? "Disabling nodejs"
+
+dnf module enable nodejs:18 -y &>> $LOGFILE
+VALIDATE $? "Enabling nodejs"
+
+dnf install nodejs -y &>> $LOGFILE
+VALIDATE $? "Installing nodejs"
+
+id roboshop #if roboshop user does not exist, then it is failure
+if [ $? -ne 0 ]
+then
+    useradd roboshop
+    VALIDATE $? "roboshop user creation"
+else
+    echo -e "roboshop user already exist $Y SKIPPING $N"
+fi
+
+mkdir -p /app  
+VALIDATE $? "Make a directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
+VALIDATE $? "Install catalogue application"
+
+cd /app &>> $LOGFILE
+VALIDATE $? "open Roboshop directory"
+
+unzip -o /tmp/catalogue.zip &>> $LOGFILE
+VALIDATE $? "Unzip catalogue application"
+
+npm install &>> $LOGFILE
+VALIDATE $? "Installing npm"
+
+cp home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>> $LOGFILE
+VALIDATE $? "Installing dependenices"
+
+systemctl daemon-reload &>> $LOGFILE
+VALIDATE $? "Daemon reload"
+
+systemctl enable catalogue &>> $LOGFILE
+VALIDATE $? "Enabling Catalogue application"
+
+systemctl start catalogue &>> $LOGFILE
+VALIDATE $? "Starting Catalogue application"
+
