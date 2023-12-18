d15ca8ec8695ef073a3c23fd8186f678a833b5e6
Harikamatta <harikamattanjc@gmail.com>
2023-12-18 07:13:31 +0530

    roboshop shell scripts

--- mongodb.sh
+++ mongodb.sh
@@ -1,43 +1,49 @@
 #!/bin/bash
+
 ID=$(id -u)
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then 
-    echo -e "$2 ...$R FAILED $N"
-    exit 1
-    else
-    echo -e " $2... $G SUCCESS $N"
-fi }
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
-echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
 
 if [ $ID -ne 0 ]
-then 
-echo -e "$R Error: : Please run the script without root user $N"
-exit    1
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
 else
-echo -e "$G Your a root user $N"
-fi #reverse of if condition
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
 
-cp mongo.repo  /etc/yum.repos.d/mongo.repo  &>> $LOGFILE
-VALIDATE $? "Copied MONGODB Repository"
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+VALIDATE $? "Copied MongoDB Repo"
 
-yum install mongodb-org -y &>> $LOGFILE
-VALIDATE $? "Installing Mongodb"
+dnf install mongodb-org -y &>> $LOGFILE
+VALIDATE $? "Installing MongoDB"
 
 systemctl enable mongod &>> $LOGFILE
-VALIDATE $? "Enabling Mongodb"
+VALIDATE $? "Enabling MongoDB"
 
-systemctl start mongod  &>> $LOGFILE
-VALIDATE $? "Starting Mongodb"
+systemctl start mongod &>> $LOGFILE
+VALIDATE $? "Starting MongoDB"
 
-sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf &>> $LOGFILE
-VALIDATE $? "Remote access to Mongodb"
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+VALIDATE $? "Remote access to MongoDB"
 
-systemctl restart mongod  &>> $LOGFILE
-VALIDATE $? "Restarting Mongodb"
+systemctl restart mongod &>> $LOGFILE
+VALIDATE $? "Restarting MongoDB"
+
