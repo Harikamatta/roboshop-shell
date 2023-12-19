9ddbee2cace0ca556a89effef263a319e1ca7c84
Harikamatta <harikamattanjc@gmail.com>
2023-12-19 14:11:43 +0530

    roboshop shell scripts

--- redis.sh
+++ redis.sh
@@ -39,7 +39,7 @@ dnf install redis -y &>> $LOGFILE
 VALIDATE $? "Installing Redisdb"
 
 
-sed -e 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf &>> $LOGFILE
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf &>> $LOGFILE
 VALIDATE $? "remote access to redisdb"
 
 systemctl enable redis &>> $LOGFILE
