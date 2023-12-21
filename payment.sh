8b5cd9dc38675b70c0f15837d08c922618039d3c
Harikamatta <harikamattanjc@gmail.com>
2023-12-21 20:53:22 +0530

    roboshop shell scripts

--- payment.sh
+++ payment.sh
@@ -1,76 +1,64 @@
 #!/bin/bash
-
 ID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-MONGDB_HOST=mongodb.daws-76s.online
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+N='\e[0m'
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
-
-echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
     if [ $1 -ne 0 ]
-    then
-        echo -e "$2 ... $R FAILED $N"
-        exit 1
+    then 
+    echo "$2 .. $R FAILED $N "
+    exit 1
     else
-        echo -e "$2 ... $G SUCCESS $N"
-    fi
+    echo "$2 .. $G SUCCESS $N"
+fi 
 }
 
 if [ $ID -ne 0 ]
-then
-    echo -e "$R ERROR:: Please run this script with root access $N"
-    exit 1 # you can give other than 0
-else
-    echo "You are root user"
-fi # fi means reverse of if, indicating condition end
-
+    then 
+    echo "$R Error: : Please ru nthe script with root access $N "
+    exit 1
+    else
+    echo "$G root user $N"
+fi 
 dnf install python36 gcc python3-devel -y &>> $LOGFILE
+VALIDATE $? "Installing python"
 
-id roboshop #if roboshop user does not exist, then it is failure
-if [ $? -ne 0 ]
-then
+id roboshop
+if [ $ID -ne 0 ]
+then 
     useradd roboshop
     VALIDATE $? "roboshop user creation"
-else
-    echo -e "roboshop user already exist $Y SKIPPING $N"
+else 
+echo "roboshop user already exist $Y SKIPPING $N"
 fi
 
-mkdir -p /app &>> $LOGFILE
+mkdir /app &>> $LOGFILE
+VALIDATE $? "creating a directory"
 
-VALIDATE $? "creating app directory"
-
-curl -L -o /tmp/payment.zip https://roboshop-builds.s3.amazonaws.com/payment.zip &>> $LOGFILE
-
-VALIDATE $? "Downloading payment"
+curl -L -o /tmp/payment.zip https://roboshop-builds.s3.amazonaws.com/payment.zip  &>> $LOGFILE
+VALIDATE $? "Downlaoding payment application"
 
 cd /app 
-
 unzip -o /tmp/payment.zip &>> $LOGFILE
-
-VALIDATE $? "unzipping payment"
+VALIDATE $? "Unzipping payment application"
 
 pip3.6 install -r requirements.txt &>> $LOGFILE
-
-VALIDATE $? "Installing Dependencies"
+VALIDATE $? "Downlaoding dependencies"
 
 cp /home/centos/roboshop-shell/payment.service /etc/systemd/system/payment.service &>> $LOGFILE
-
-VALIDATE $? "Copying payment service"
+VALIDATE $? "Copying payment file"
 
 systemctl daemon-reload &>> $LOGFILE
-
-VALIDATE $? "daemon reaload"
+VALIDATE $? "payment Daemon Reload"
 
 systemctl enable payment  &>> $LOGFILE
-
-VALIDATE $? "Enable payment"
+VALIDATE $? "Enabling payment"
 
 systemctl start payment &>> $LOGFILE
-
-VALIDATE $? "Start payment"
+VALIDATE $? ""Starting payment" 
